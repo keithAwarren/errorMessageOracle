5a9e0107532172e319d41dc4bee14016e5af16f6
Keith Warren <128069942+keithAwarren@users.noreply.github.com>
2025-04-28 17:49:44 -0230

    preload.js comments

--- preload.js
+++ preload.js
@@ -1,5 +1,8 @@
+// Electron security modules
 const { contextBridge, ipcRenderer } = require("electron");
 
+// Safely expose Oracle access to the frontend
 contextBridge.exposeInMainWorld("oracle", {
+  // Ask the Oracle by sending errorText to the main process
   ask: (errorText) => ipcRenderer.invoke("invoke-oracle", errorText),
 });
